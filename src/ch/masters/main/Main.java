db9ac6c97866b095442dbdd26b6850a8db40a8d3
Phong6698 <Phong@hotmail.ch>
2016-06-21 15:27:31 +0200

    Parsing

--- src/ch/masters/main/Main.java
+++ src/ch/masters/main/Main.java
@@ -1,13 +1,17 @@
 package ch.masters.main;
 
+import ch.masters.manager.TrainDepartureAdmin;
+import ch.masters.model.Departure;
 import ch.masters.utils.DataScanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		DataScanner dataScanner = new DataScanner();
-//		URL file = Main.class.getResource("resources/data/abfahrten_zhb.csv");
-		dataScanner.getFile("resources/data/abfahrten_zhb.csv");
+		TrainDepartureAdmin trainDepartureAdmin = new TrainDepartureAdmin();
+		
+		for(Departure departure : trainDepartureAdmin.getAllDepartures()){
+			System.out.println(departure.getName());
+		}
 	}
 
 }
--- src/ch/masters/manager/TrainDepartureAdmin.java
+++ src/ch/masters/manager/TrainDepartureAdmin.java
@@ -1,8 +1,11 @@
 package ch.masters.manager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import ch.masters.model.Departure;
+import ch.masters.utils.DataScanner;
 
 public class TrainDepartureAdmin {
 	
@@ -12,12 +15,28 @@ public class TrainDepartureAdmin {
 	 * @return ArrayList out of Departures
 	 */
 	public ArrayList<Departure> getAllDepartures(){
+		ArrayList<Departure> departures = new ArrayList<>();
+		DataScanner dataScanner = new DataScanner();
+		BufferedReader br = dataScanner.getFile("resources/data/abfahrten_zhb.csv");
+		
 		String line = "";
 	    String cvsSplitBy = ";";
-	    while ((line = br.readLine()) != null) {
-	      String[] data = line.split(cvsSplitBy);
-	      System.out.println(data[1]);
-	    }
+	    try {
+	    	br.readLine(); //skip first line
+			while ((line = br.readLine()) != null) {
+				Departure departure = new Departure();
+				String[] data = line.split(cvsSplitBy);
+				departure.setName(data[0]);
+				departure.setDeparture(data[1]);
+				departure.setTo(data[2]);
+				departure.setVia(data[3]);
+				departure.setPlatform(data[4]);	
+				departures.add(departure);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    return departures;
 	}
 	
 	/**
