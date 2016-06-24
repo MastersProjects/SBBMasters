01fced1a141f096c3864d4d18065802bebdfdaf8
Chiramet Phong Penglerd <Phong@hotmail.ch>
2016-06-24 09:53:02 +0200

    Listener :ear: @zperee
    @zperee please do:
    - create package .listener and put this class in it
    - add those getter and setter in SBBMasters.java
    
    ```Java
    public JTable getTable() {
    		return table;
    	}
    
    
    	public void setTable(JTable table) {
    		this.table = table;
    	}
    
    
    	public JTextField getTime1() {
    		return time1;
    	}
    
    
    	public void setTime1(JTextField time1) {
    		this.time1 = time1;
    	}
    
    
    	public JTextField getPlatform() {
    		return platform;
    	}
    
    
    	public void setPlatform(JTextField platform) {
    		this.platform = platform;
    	}
    
    
    	public JTextField getTime2() {
    		return time2;
    	}
    
    
    	public void setTime2(JTextField time2) {
    		this.time2 = time2;
    	}
    
    
    	public JTextField getVia() {
    		return via;
    	}
    
    
    	public void setVia(JTextField via) {
    		this.via = via;
    	}
    ```

---
+++ src/ch/masters/SBBMastersListener.java
@@ -0,0 +1,128 @@
+package ch.masters.listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import ch.masters.manager.TrainDepartureAdmin;
+import ch.masters.model.Departure;
+import ch.masters.utils.ValidationChecker;
+import ch.masters.view.SBBMasters;
+
+/**
+ * ActionListener for the SBBMasters View
+ * @author Chiramet Phong Penglerd, Elia Perenzin
+ * @year 2016
+ * @version 1.0
+ */
+public class SBBMastersListener implements ActionListener{
+
+	private SBBMasters sbbMasters;
+	private TrainDepartureAdmin trainDepartureAdmin;
+	private ValidationChecker validationChecker;
+	
+	public SBBMastersListener(SBBMasters sbbMasters) {
+		this.sbbMasters = sbbMasters;		
+		trainDepartureAdmin = new TrainDepartureAdmin();
+		validationChecker = new ValidationChecker();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		//Button Departure
+		if(arg0.getActionCommand().equals("Departures")){
+			sbbMasters.getTable().setModel(getDepartureTableModel(trainDepartureAdmin.getAllDepartures()));
+			emptyTestFields();
+			
+		//Button Departure by time
+		}else if(arg0.getActionCommand().equals("Departures by time")){
+			
+			//Validation check
+			if(validationChecker.checkTimeFormat(sbbMasters.getTime1().getText())){
+				sbbMasters.getTable().setModel(getDepartureTableModel(trainDepartureAdmin.getDepartures(sbbMasters.getTime1().getText())));
+				emptyTestFields();
+			}else{
+				//TODO Message
+			}
+			
+		//Button Departure by platform and time
+		}else if(arg0.getActionCommand().equals("Departures by platform and time")){
+			
+			//Validation check
+			if(!validationChecker.checkEmpty(sbbMasters.getPlatform().getText()) && validationChecker.checkTimeFormat(sbbMasters.getTime2().getText())){
+				sbbMasters.getTable().setModel(getDepartureTableModel(trainDepartureAdmin.getPlatformDepartures(sbbMasters.getPlatform().getText(), sbbMasters.getTime2().getText())));
+				emptyTestFields();
+			} //TODO Message
+			
+		//Button Departure by via
+		}else if(arg0.getActionCommand().equals("Departures by via")){
+			
+			//Validation check
+			if(!validationChecker.checkEmpty(sbbMasters.getVia().getText())){
+				sbbMasters.getTable().setModel(getDepartureTableModel(trainDepartureAdmin.getDeparturestToCity(sbbMasters.getVia().getText())));
+				emptyTestFields();
+			}else{
+				//TODO Message
+			}
+		}
+		
+	}
+	
+	
+	/**
+	 * Function for empty the TextFields
+	 */
+	private void emptyTestFields(){
+		sbbMasters.getTime1().setText("");
+		sbbMasters.getTime2().setText("");
+		sbbMasters.getPlatform().setText("");
+		sbbMasters.getVia().setText("");
+	}
+	
+	
+	/**
+	 * Creates TableModel for departures Table
+	 * @param departures
+	 * @return TableModel
+	 */
+	private TableModel getDepartureTableModel(ArrayList<Departure> departures){
+		/*
+		 * Model for JTables
+		 */
+		//Vector mit den Titeln erstellen
+		Vector<String> columnNames = new Vector<String>();
+		columnNames.addElement("Line");
+		columnNames.addElement("Departure Time");
+		columnNames.addElement("To");
+		columnNames.addElement("Via");
+		columnNames.addElement("Platform");
+	
+		//Vector mit den Datensätze erstellen
+		@SuppressWarnings("rawtypes")
+		Vector<Vector> data = new Vector<Vector>();
+		
+		//erstellt ein Vector mit den Noten
+		for(Departure departure : departures){
+			
+			Vector<Object> row = new Vector<Object>();
+				
+			row.addElement(departure.getName());	
+			row.addElement(departure.getDeparture());
+			row.addElement(departure.getTo());
+			row.addElement(departure.getVia());
+			row.addElement(departure.getPlatform());
+			
+			data.addElement(row);						
+		}
+		
+		//Tabel Model erstellen
+		TableModel model = new DefaultTableModel(data, columnNames);
+		return model;
+	}
+
+}
